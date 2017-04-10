8973429dc1b2ba4ffcd4f399fa470a9db0da7886
JayFu <382603080@qq.com>
2017-04-10 09:51:00 +0800

    输出bug

--- factorial.py
+++ factorial.py
@@ -1,17 +1,25 @@
 #输入整数n，计算s=1!+2!……+n!的末尾六位数
+#期望输入：20        40
+#期望输出：820313    940313
 n = input()
+n = int(n)
 #初始化数据
-b = 1
-s = 0
+
+x = 0
+#j = 1
 #计算阶乘之和模块
-for i in n:
-    for j in n:
-        b = b * j
-        s = s + b
+for i in range(n):
+    i += 1
+    b = 1
+    for j in range(i):
+        b *= (j+1)
+    x += b
+#    print('x=',x)
 
 #将阶乘之和末尾六位数字输出
-m = str(s)
-for j in 6:
-    j = 0 - j
-    print(m[j])
+m = str(x)
+for j in range(6):
+    j = 0 - (j + 6)
+    print(m[j], end='')
 
+#输入20时输出出现811149%
