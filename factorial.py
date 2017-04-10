f3a62ee34488d34ee1fdc83acad6c43fa3eaf3df
JayFu <382603080@qq.com>
2017-04-10 09:50:32 +0800

    commit bug1

--- factorial.py
+++ factorial.py
@@ -1,13 +1,17 @@
 #输入整数n，计算s=1!+2!……+n!的末尾六位数
 n = input()
-
+#初始化数据
+b = 1
+s = 0
 #计算阶乘之和模块
 for i in n:
     for j in n:
         b = b * j
-    s = s + b
+        s = s + b
 
 #将阶乘之和末尾六位数字输出
 m = str(s)
-print(m[-6]m[-5]m[-4]m[-3]m[-2]m[-1])
+for j in 6:
+    j = 0 - j
+    print(m[j])
 
