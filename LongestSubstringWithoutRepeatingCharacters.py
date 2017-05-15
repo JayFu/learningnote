33dc76378cfe81d0caf4993f9de01fe0d32068ce
JayFu <382603080@qq.com>
2017-05-15 18:39:57 +0800

    【深浅复制】字符串中的最长不重复子字符串问题。

--- LongestSubstringWithoutRepeatingCharacters.py
+++ LongestSubstringWithoutRepeatingCharacters.py
@@ -9,26 +9,45 @@ class Solution(object):
         SubList1 = []
         SubList2 = []
         for i in range(len(s)):
-            if not (s[i] in SubList1): SubList1.append(s[i])
-            elif len(SubList1) > len(SubList2):
-            SubList2 = SubList1
-            SubList1 = []
-            SubList1.append(s[i])
-        else: SubList1 = []
+            if not (s[i] in SubList1): 
+                SubList1.append(s[i])
+            else:
+                while(s[i] in SubList1): 
+                    SubList1.pop(0)
+                SubList1.append(s[i])
+            continue
+            if len(SubList1)>len(SubList2):
+                SubList2 = SubList1[:]
+        if SubList2 == [] : SubList2 = SubList1
         return len(SubList2)
 
-#s = 'abcabcab'
-#s = 'pwwkew'
-#SubList1 = []
-#SubList2 = []
-#for i in range(len(s)):
-#    if not (s[i] in SubList1): SubList1.append(s[i])
-#    elif len(SubList1) > len(SubList2):
-#        SubList2 = SubList1
- #       SubList1 = []
-  #      SubList1.append(s[i])
-   # else:
-    #    SubList1 = []
-#    print(SubList1)
-#    print(SubList2)
-#print(len(SubList2))
+
+# import copy
+# s = 'abcabcab'
+# s = 'qwer'
+# s = 'pwwkew'
+
+# s = 'abcabcbbb'
+#期望输出3
+
+# s = 'aab'
+# s = 'cbb'
+# s = 'dvdf'
+# SubList1 = []
+# SubList2 = []
+# for i in range(len(s)):
+#     if not(s[i] in SubList1):
+#         SubList1.append(s[i])
+#     else:
+#         while(s[i] in SubList1): 
+#             SubList1.pop(0)
+#         SubList1.append(s[i])
+#         continue
+#     if len(SubList1)>len(SubList2):
+#         # SubList2 = SubList1
+#        #等号是深复制，这里应该使用copy
+#        SubList2 = SubList1.copy()
+        #LeetCode网站使用的是python2，此时应使用
+        #SubList2 = SubList[:]
+# if SubList2 == []: SubList2 = SubList1
+# print(len(SubList2))
