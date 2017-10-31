d4836832fe72ba6d2a8b1bc2944d3112a747574b
JayFu <382603080@qq.com>
2017-10-31 14:39:57 +0100

    链表相加

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -1,7 +1,8 @@
-#来自LeetCode的另一题，考察了链表操作。
-#将两个保存在链表中的数相加，除了0本身以外不使用0开头。
-#Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-#Output: 7 -> 0 -> 8
+# 将两个链表相加，得到一个链表，进位在下一个结点+1
+# 链表的每个结点包含一个数字
+
+# 输入: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# 输出: 7 -> 0 -> 8
 
 # Definition for singly-linked list.
 # class ListNode(object):
@@ -9,12 +10,83 @@
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        
+# class Solution(object):
+#     def addTwoNumbers(self, l1, l2):
+#         """
+#         :type l1: ListNode
+#         :type l2: ListNode
+#         :rtype: ListNode
+        # """
 
+class ListNode(object):      
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+p = l1
+q = l2
+OutputNodeList = ListNode(0)
+carry = 0
+
+lenA = 1
+lenB = 1
+while l1.next is not None:
+    lenA = lenA + 1
+    l1 = l1.next
+
+while l2.next is not None:
+    lenB = lenB + 1
+    l2 = l2.next
+
+llen = max(lenA, lenB)
+
+print(llen)
+# while p.next and q.next is not None:
+# 报错"'NoneType' object has no attribute 'val'"
+# 可能存在数据越界
+# 原因是and和is not的优先级没有按照顺序
+
+# OutputNodeList必须被从头到尾初始化
+# 可以考虑从l1和l2中选择长的那个进行深复制
+# 复制之后从第一个结点开始初始化为0，再加一个val为0的结点
+# 对这个初始化之后的链表进行每个结点的数值操作
+
+while (p is not None) and (q is not None):
+    OutputNodeList.val = p.val + q.val + carry
+    if OutputNodeList.val >= 10:
+        carry = 1
+        OutputNodeList.val = OutputNodeList.val - 10
+    else:
+        carry = 0
+    p = p.next
+    q = q.next
+    OutputNodeList = OutputNodeList.next
+
+if p.next is not None:
+    while p.next is not None:
+        OutputNodeList.val = p.val + carry
+        if OutputNodeList.val >= 10:
+            carry = 1
+            OutputNodeList.val = OutputNodeList.val - 10
+        else:
+            carry = 0
+elif q.next is not None:
+     while q.next is not None:
+        OutputNodeList.val = q.val + carry
+        if OutputNodeList.val >= 10:
+            carry = 1
+            OutputNodeList.val = OutputNodeList.val - 10
+        else:
+            carry = 0
+elif carry is not None: OutputNodeList.val = carry
+else: pass
+
+print(OutputNodeList.val)
--- processtest.py
+++ processtest.py
@@ -118,55 +118,58 @@
 
 
 #单链表的实现
-# class LNode:
-#     def __init__(self, elem, next_=None):
-#         self.elem = elem
-#         self.next = next_
+class LNode:
+    def __init__(self, elem, next_=None):
+        self.elem = elem
+        self.next = next_
 #简单的表结点类
+# 引入一个LeetCode上的链表问题“AddTwoNumbers”，见同文件夹文件
 
-# llist1 = LNode(1)
-# p = llist1
-# for i in range(2,11):
-#     p.next = LNode(i)
-#     p = p.next
-# #p = p.next 是遍历的基础方法
+llist1 = LNode(1)
+p = llist1
+for i in range(2,13):
+    p.next = LNode(i)
+    print("step",i,p.elem)
+    p = p.next
+print(p.elem, llist1.next.elem)
+#p = p.next 是遍历的基础方法
 
-# p = llist1
-# while p is not None:
-#     # print(p.elem) 遍历内容中的基础操作，以输出作为例子
-#     pass
-#     p = p.next
-# p = llist1
-# if p.next = None: return print("List is void")
-# #判空
+p = llist1
+while p is not None:
+    # print(p.elem) 遍历内容中的基础操作，以输出作为例子
+    pass
+    p = p.next
+p = llist1
+if p.next = None: return print("List is void")
+# 判空
 
-# p = llist1
-# q = LNode(13)
-# q.next = p
-# p = q
-# #首端插入
+p = llist1
+q = LNode(13)
+q.next = p
+p = q
+#首端插入
 
-# p = llist1
-# for i in range(2,11):
-#     p.next = LNode(i)
-#     p = p.next
-# q = LNode(13)
-# while p.next is not None:
-#     if p.elem = 5:
-#         q.next = p.next
-#         p.next = q
-#     else: p = p.next
-# #中间插入
+p = llist1
+for i in range(2,11):
+    p.next = LNode(i)
+    p = p.next
+q = LNode(13)
+while p.next is not None:
+    if p.elem = 5:
+        q.next = p.next
+        p.next = q
+    else: p = p.next
+#中间插入
 
 # head = head.next
 # xxx.next = xxx.next.next
-# #首端、中间删除
+# 首端、中间删除
 
-# def a(b):
-    # pass
-# b = LNode(123)
-# if a(b.elem):
-#     print('11')
+def a(b):
+    pass
+b = LNode(123)
+if a(b.elem):
+    print('11')
 
 
 
@@ -242,36 +245,146 @@
 
 
 
-#正则表达式主要操作
-import re
-r1 = re.compile("abc")
-#生成与abc对应的正则表达式对象并赋给r1
-re.search(r1, "abcabcababbabaca", flag=0)#书上的标记为flag = 0,但是python自动弹出的是flags = 0,书上提及该功能不重要，将不涉及
-#在“abcabcababbabaca”中检索与r1匹配的子串
-#成功返回一个match类型对象，否则None。
-re.match(r1, "aavcccavaavacxb")
-#在"aavcccavaavacxb"中检索与r1匹配的前缀
-#成功返回一个match类型对象，否则None
-re.split(' ',"abc sfd asa ddf ert",maxsplit=0, flags=0)
-#从"abc sfd asa ddf ert"将空格作为分割符，得到
-# ['abc', 'sfd', 'asa', 'ddf', 'ert']
-#maxsplit表示最大分割数，默认，即0表示处理完整个string
-re.split(' ', '1 2 3  4    5')
-# ['1','2','3',' ','4',' ',' ',' ','5']
-re.findall(pattern, string, flags=0)
-#返回一个表，包含找到的所有匹配的子串
+# #正则表达式主要操作
+# import re
+# r1 = re.compile("abc")
+# #生成与abc对应的正则表达式对象并赋给r1
+# re.search(r1, "abcabcababbabaca", flag=0)#书上的标记为flag = 0,但是python自动弹出的是flags = 0,书上提及该功能不重要，将不涉及
+# #在“abcabcababbabaca”中检索与r1匹配的子串
+# #成功返回一个match类型对象，否则None。
+# re.match(r1, "aavcccavaavacxb")
+# #在"aavcccavaavacxb"中检索与r1匹配的前缀
+# #成功返回一个match类型对象，否则None
+# re.split(' ',"abc sfd asa ddf ert",maxsplit=0, flags=0)
+# #从"abc sfd asa ddf ert"将空格作为分割符，得到
+# # ['abc', 'sfd', 'asa', 'ddf', 'ert']
+# #maxsplit表示最大分割数，默认，即0表示处理完整个string
+# re.split(' ', '1 2 3  4    5')
+# # ['1','2','3',' ','4',' ',' ',' ','5']
+# re.findall(pattern, string, flags=0)
+# #返回一个表，包含找到的所有匹配的子串
 
-#match对象
-#match对象可以用于逻辑判断，搜索到即为真，比如
-if match111 = re.search('sda', 'sssssad'):pass
-#match还有一些可供使用的方法
-match111.group()
-#取得被匹配的子串
-match111.start()
-#取得被匹配的子串的开始位置
-match111.end()
-#取得被匹配的子串的结束位置
-#存在 mat.group() == text[mat.start() : mat.end()]
-match111.span()
-#得到由开始位置和结束位置形成的二元组
-#存在 mat.span() == mat.start() , mat.end()
+# #match对象
+# #match对象可以用于逻辑判断，搜索到即为真，比如
+# if match111 = re.search('sda', 'sssssad'):pass
+# #match还有一些可供使用的方法
+# match111.group()
+# #取得被匹配的子串
+# match111.start()
+# #取得被匹配的子串的开始位置
+# match111.end()
+# #取得被匹配的子串的结束位置
+# #存在 mat.group() == text[mat.start() : mat.end()]
+# match111.span()
+# #得到由开始位置和结束位置形成的二元组
+# #存在 mat.span() == mat.start() , mat.end()
+
+
+
+# 栈和队列
+
+# Python中的list就提供了栈相关的操作，如lst.append(x)和lst.pop()以及lst[-1]
+# 但是，这样建立的对象仍旧是list,而且提供了很多不应该出现的操作，威胁了栈的安全性
+# 可以基于顺序表定义一个栈类
+# class SStack():
+#     def __init__(self):
+#         self._elems = []
+    
+#     def is_empty(self):
+#         return self._elems == []
+
+#     def top(self):
+#         if self._elems == []: raise StackUnderflow('in SStack.top()')
+#         return self._elems[-1]
+
+#     def push(self,elem):
+#         self._elems.append(elem)
+    
+#     def pop(self):
+#         if self._elems == []: raise StackUnderflow("in SStack.pop()")
+#         return self._elems.pop()
+# 由于扩大储存需要一次高代价操作，加上顺序表需要完整的大块存储区，所以也可以使用链接
+# 链接的缺点在于链接开销和在内存中任意散布结点带来的操作开销
+# 如果使用链接表实现栈，较正常的链接表实现简单化
+# 结点定义转至120行——125行
+# class LStack():
+#     def __init__(self):
+#         self._top = None
+    
+#     def is_empty(self):
+#         return self._top is None
+
+#     def top(self):
+#         if self._top is None: raise StackUnderflow("in LStack.top()")
+#         return self._top.elem
+
+#     def push(self, elem):
+#         self._top = LNode(elem, self._top)
+    
+#     def pop(self):
+#         if self._top is None: raise StrackUnderflow("in LStack.pop()")
+
+#         p = self._top
+#         self._top = p.next
+#         return p.elem
+
+# 考虑用栈实现递归计算阶乘
+
+
+
+
+#创建一个基于顺序表的队列类
+# class LLList():
+#     def __init__(self):
+#         self._elems = []
+    
+#     def is_empty(self):
+#         if self._elems = []: raise ListUnderflow("in LLList.is_empty") #print("error in is_empty()")
+
+#     def append(self, elems):
+#         return self._elems.append(elems)
+
+#     def pop(self):
+#         self.is_empty(self)
+#         temp = self._elems[0]
+#         for i in len(self._elems) + 1: self._elems[i] = self._elems[i + 1]
+#         return temp
+
+#     def top(self):
+#         self.is_empty
+#         return self._elems[0]
+
+# 从上面的pop操作中可以看出，对于基于顺序表的队列类，每次进入或者弹出操作有一个操作需要移动整个数列，所以使用链表结构更加节省时间
+
+# 创建一个基于链表的队列结构
+# 结点定义转至120行-125行
+
+# class NLList():
+#     def __init__(self):
+#         self._top = None
+    
+#     def is_empty(self):
+#         return self._top is None
+
+#     def top(self):
+#         if self._top is None: raise ListUnderflow("in NLList.top()")
+#         return self._top
+
+#     def pop(self):
+#         p = self.top
+#         while p.next is NOT None : p = p.next
+#         temp = p
+#         # 这里发现需要弹出最后一个元素还需要向前的指针，单向链表可能无法满足需求
+
+#         # 或者控制while函数遍历到倒数第二个结点
+#         while p.next.next is not None: p = p.next
+#         temp = p.next
+#         p.next = None
+#         return temp
+
+#     def append(self,elemt):
+#         if self.is_empty : self._top = elemt
+#         else : 
+#             p = self.top
+#             self.top = elemt
+#             self.top.next = p
