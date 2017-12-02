7a9582cabe466a03c1d6907d61c2d1439b71e60d
JayFu <382603080@qq.com>
2017-12-02 17:58:46 +0100

    补充投票脚本

--- toupiao.py
+++ toupiao.py
@@ -2,8 +2,8 @@ import requests
 
 class tooupiao():
     def __init__(self):
-        self.UUUrl = 'http://weike.enetedu.com/js_support.asp?vodid=187851_5&xiangmu=22&nxxx=0.6479469329751237'
-
+        # self.UUUrl = 'http://weike.enetedu.com/js_support.asp?vodid=187851_5&xiangmu=22&nxxx=0.6479469329751237'
+        self.UUUrl = 'http://weike.enetedu.com/js_useradopt.asp?vodid=187851&xiangmu=22&nzz=0.3306213882457867'
     def tttou(self):
         Hheader = {
             'User-Agent':'User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'
