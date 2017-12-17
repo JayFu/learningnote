5dbcd3161f653bbac76526077964c67abfa835bb
JayFu <382603080@qq.com>
2017-12-17 22:12:49 +0100

    12月17晚

---
+++ KeysKeyboard.py
@@ -0,0 +1,25 @@
+# Initially on a notepad only one character 'A' is present. You can perform two operations on this notepad for each step:
+
+# Copy All: You can copy all the characters present on the notepad (partial copy is not allowed).
+# Paste: You can paste the characters which are copied last time.
+# Given a number n. You have to get exactly n 'A' on the notepad by performing the minimum number of steps permitted. Output the minimum number of steps to get n 'A'.
+
+# class Solution(object):
+#     def minSteps(self, n):
+#         """
+#         :type n: int
+#         :rtype: int
+#         """
+      
+def minSteps(n):
+    """
+    """
+    zed, zoe, cait = 0, 0, 0
+    while zoe is not n:
+        cait += 1
+        if pass:
+            zed = zoe
+        if pass:
+            zoe += zed
+    
+    return cait
