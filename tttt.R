f2e3f62e599ef88cb47ae30cb213eb53f544c6a0
JayFu <382603080@qq.com>
2018-01-15 00:03:14 +0100

    逻辑回归模型

--- tttt.R
+++ tttt.R
@@ -1,207 +1,189 @@
-#As a first step, clear your environment from any existing data
+# clear your environment from any existing data
 rm(list = ls())
 
-#clear existing plots (if any)
+# clear existing plots
 dev.off()
 
-#First, we will start by loading our dataset. As a start we will use a default dataset provided by R
+# First, we will start by loading our dataset
 Heart <- read.csv(file.choose())
-#The dataset is called Heart:
-help(Heart)
-
-#always make a copy of the data to work on to have a backup ready for your original data!
 data <- Heart
-head(data)
-dim(data)
-summary(data)
+# Logistic Regression part
 
-#Cluster your data for discovering the natural groups (iris species)
-install.packages("ggplot2")
-library(ggplot2)
-
-qplot(ID, Age, Sex, RestBP, Chol, Fbs, RestECG, MaxHR, ExAng, Oldpeak, Slope, Ca, data = Heart)
-
-#For this part we will use decision trees and random forests. Start by installing these packages
-#these packages are used to build a CART: Classification and Regression Tree
-install.packages("rpart")
-library(rpart)
-
-install.packages("caret")
-library(caret)
-
-#before we build our models, we will divide our dataset into a training set and a test set to avoid 
-#overfitting. The training set is used to build the model and the test set is used to validate it
-
-#we start by using this function from the CARET package to partition our dataset
-#we set p: the percentage of data that goes to training into 0.7, thus, 70% of data is used for
-#training to capture the main characterstics and the other 30% is used for validation
-train.flag <- createDataPartition(y = Heart$ID, p=0.7, list=FALSE)
-
-#set the training data
-training <- Heart[train.flag,]
-
-#set the left data to validation
-Validation <- Heart[-train.flag,]
-
-#before we go on and build our models, we want to make sure to implement the cross validation 
-#to avoid overfitting
-control <- trainControl(method = "cv", number=10)
-metrix <- "Accuracy"
-
-#now we build the model using the training data
-#in the first part, we specifiy our dependent variable (Species) ~ . the dot represents all the other
-#explanatory variables in the dataset which can be rewritten as Species ~ Sepal.Length + Sepal.Width + ..
-#the method rpart is Recursive Partitioning and Regression Trees which builds the decision tree
-#we also make sure to add metric and trControl attributes to build the model with 10-fold cross validation
-#before training each model, we will use the set.seed function to make sure our results are reproducable!
-
+# split data
 set.seed(10)
-tree_model <- train(ID ~ ., method="rpart", data=training, metric=metrix, trControl=control)
-
-#summarize the model
-summary(tree_model)
-tree_model
-
-#to plot the tree model we will use the following package
-install.packages("rattle")
-library(rattle)
-
-fancyRpartPlot(tree_model$finalModel)
-
-#now that we built the model, we need to validate it
-#to do so, we use the predict function
-#as a first step, we will try to predict with the same data we trained our model with
-train.cart <- predict(tree_model, newdata = training)
-
-#here we will show the original values vs the predicted ones
-table(train.cart, training$ID)
-
-#now we will predict using the test set
-pred.cart <- predict(tree_model, newdata = Validation)
-
-#here we will show the original values vs the predicted ones
-table(pred.cart, Validation$ID)
-
-#now let's try to plot the missclassified observations 
-correct <- pred.cart == Validation$ID
-
-qplot(ID, Age, Sex, RestBP, Chol, Fbs, RestECG, MaxHR, ExAng, Oldpeak, Slope, Ca, data = Heart)
-
-#calculate the confusion matrix
-confusionMatrix(pred.cart, Validation$ID)
-
-#now we will implement a Random Forest model using the following packages
-install.packages("randomForest")
-library(randomForest)
-
-install.packages("randomForestSRC")
-library(randomForestSRC)
-
-#now we build the model using the training dataset as before
-#don't forget to set the seed
-
-set.seed(10)
-random_forest_model <- train(ID ~ ., method="rf", data=training, metric=metrix, trControl=control)
-
-#summarize the random forest model
-summary(random_forest_model)
-random_forest_model
-
-#in our first model, we were able to show the decision tree. However, in the case of a random forest
-#we look more for variables importance to be able to decide which variables are most imprtant and
-#which ones can be ignored. For that we will use varImp function
-
-var_importance <- varImp(random_forest_model)
-plot(var_importance)
-
-#predict using training data
-train.rf <- predict(random_forest_model, newdata = training)
-
-#show the results 
-table(train.rf, training$ID)
-
-#now we will predict using the test set
-pred.rf <- predict(random_forest_model, newdata = Validation)
-
-#here we will show the original values vs the predicted ones
-table(pred.rf, Validation$ID) 
-
-#calculate the confusion matrix
-confusionMatrix(pred.rf, Validation$ID)
-
-#now let's compare the two models using the resamples function which enables us to combine the
-#results from each model and plot them!
-
-results <- resamples(list(cart = tree_model, rf = random_forest_model))
-summary(results)
-dotplot(results)
-
-#For this part of the tutorial, we will use Logistic Regression 
-#as a start, we want to create a subset of the iris dataset. The reason is because we want to
-#demonstrate the logistic regression which is applicable on a binary dependent variable, however,
-#the dependent variable (Species) contains three possible values, so we will take a small subset
-#that contains verginica and versicolor species
-attach(Heart)
-
-#install and call this package to use the filter function to subset your data
-install.packages("dplyr")
-library(dplyr)
+train <- createDataPartition(data$AHD, p = 0.7, list = FALSE)
+trainning <- data[train, ]
+test  <-  data[-train, ]
+# fit and summary
+LRModel <- train(AHD ~., data = trainning, method = "glm", family = "binomial")
+summary(Model)
+# predict
+fitted.results <- predict(LRModel,newdata = test,type='prob')
+fitted.results <- ifelse(fitted.results > 0.5,1,0)
+misClasificError <- mean(fitted.results != test$AHD)
+print(paste('Accuracy',1-misClasificError))
 
 
+# help(Heart)
 
-#now we build the model using glm function. GLM stands for generalized linear models. 
-#we set the family to binomial with a logit link to specifiy the model we want which is logstic regression
-logit_model <- glm(ID ~ ., data = Heart, family = binomial(link="logit"))
+# # make a copy of the data to work on 
+# data <- Heart
+# # pre-process data
+# head(data)
+# dim(data)
+# summary(data)
 
-#summarize the model
-summary(logit_model)
+# # download and install packages
+# install.packages("rpart")
+# install.packages("caret")
+# install.packages("plotly")
+# install.packages("rattle")
+# install.packages("randomForest")
+# install.packages("randomForestSRC")
+# install.packages("dplyr")
+# install.packages("ggplot2")
+# install.packages("ROCR")
+# library(ROCR)
+# library(ggplot2)
+# library(dplyr)
+# library(randomForest)
+# library(randomForestSRC)
+# library(rattle)
+# library(rpart)
+# library(caret)
+# library(plotly)
 
-#looking at the summary, we can have an idea about the p-values of the explanatory variables,
-#thus, the significane of such variables in our model. let's try to remove the variables with higher 
-#p-values and see what happens. p-values that are higher than 0.1 are mostly considered insignificant
-logit_model_reduced <- glm(ID ~ .,data = Heart, family = binomial(link = "logit"))
+# # clusters the data based on disease or not
+# p <- plot_ly(data, x = data$Age, y = data$AHD, 
+# color = data$AHD, colors = "Set1", mode = "markers")
+# p
 
-#we can do that also by simply using the step function on the full model
-step(logit_model)
+# # set.seed function to make sure our results are reproducable
+# set.seed(10)
 
-#now summarize again
-summary(logit_model_reduced)
+# # we will divide our dataset into a training set and a test set 
+# train.flag <- createDataPartition(y = Heart$AHD, p=0.7, list=FALSE)
+# training <- Heart[train.flag,]
+# Validation <- Heart[-train.flag,]
 
-#now let's plot the fitted probability as a function of the linear predictor
-lr_data <- data.frame(predictor=logit_model_reduced$linear.predictors, prob=logit_model_reduced$fitted.values, ID=Heart$ID)
-ggplot(lr_data, aes(x=predictor, y=prob, color=Species)) + geom_point()
+# # before we go on and build our models, we want to make sure to implement the cross validation 
+# # to avoid overfitting
+# control <- trainControl(method = "cv", number=10)
+# metrix <- "Accuracy"
 
-#partition our new dataset
-train.flag_sub <- createDataPartition(y = Heart$ID, p=0.7, list=FALSE)
+# # we specifiy our dependent variable
+# # add metric and trControl attributes to build the model with 10-fold cross validation
 
-#set the training data
-training_sub <- Heart[train.flag_sub,]
+# tree_model <- train(Age ~ ., method="rpart", data=training, metric=metrix, trControl=control)
 
-#set the left data to validation
-Validation_sub <- Heart[-train.flag_sub,]
+# #summarize the model
+# summary(tree_model)
+# tree_model
 
-#now let's predict
-install.packages("ROCR")
-library(ROCR)
+# fancyRpartPlot(tree_model$finalModel)
 
-pred.logit <- predict(logit_model_reduced, newdata = Validation_sub, type = "response")
+# # validate the model
+# # predict with the same data we trained our model with
+# train.cart <- predict(tree_model, newdata = training)
 
-#the results we get from the prediction is a probability of a specific category to occur, accordingly
-#we add prob as a 6th variable to the validation dataset so that we can compare between the predicted
-#value and the original one
-Validation_sub$prob <- pred.logit
+# # show the original values vs the predicted ones
+# table(train.cart, training$AHD)
 
-#we can see that the original values are wither 0 or 1 while the probability is a value between 0 and 1
-#accordingly, we use the prediction function to transform the probabilities into a standard format
-my_pred <- prediction(Validation_sub$prob, Validation_sub$ID)
+# # predict using the test set
+# pred.cart <- predict(tree_model, newdata = Validation)
 
-#now, to evaluate out predictions, we are going to use the performance funtion and calculate two 
-#values: tpr is true positive rate which indicates the ratio of predictions that were correctly
-#classified and fpr which is false positive rate which indicates the ratio of predictions that were
-#misclassified. These value will produce our ROC curve which is used to evaluate our model
-my_roc<- performance(my_pred, "tpr", "fpr")
-plot(my_roc)
+# # show the original values vs the predicted ones
+# table(pred.cart, Validation$AHD)
 
-#now we use the same function to calculate the accuracy
-my_auc <- performance(my_pred, measure = "auc")
-my_auc@y.values
+# # plot the missclassified observations 
+# correct <- pred.cart == Validation$AHD
+
+# p <- plot_ly(Validation, x = data$Age, y = data$AHD, 
+# color = correct, colors = "Set1", mode = "markers")
+# p
+
+# #calculate the confusion matrix
+# confusionMatrix(pred.cart, Validation$AHD)
+
+# # build the model using the training dataset
+# set.seed(10)
+# random_forest_model <- train(Age ~ ., method="rf", data=training, metric=metrix, trControl=control)
+
+# # summarize the random forest model
+# summary(random_forest_model)
+# random_forest_model
+
+# # use varImp function to decide which variables are most imprtant and which ones can be ignored
+# var_importance <- varImp(random_forest_model)
+# plot(var_importance)
+
+# # predict using training data
+# train.rf <- predict(random_forest_model, newdata = training)
+
+# # show the results 
+# table(train.rf, training$AHD)
+
+# # now we will predict using the test set
+# pred.rf <- predict(random_forest_model, newdata = Validation)
+
+# # here we will show the original values vs the predicted ones
+# table(pred.rf, Validation$AHD) 
+
+# # calculate the confusion matrix
+# confusionMatrix(pred.rf, Validation$AHD)
+
+# # compare the two models 
+# results <- resamples(list(cart = tree_model, rf = random_forest_model))
+# summary(results)
+# dotplot(results)
+
+
+# # Logistic Regression 
+# # don't have to create a subset of Heart dataset for AHD has only two possible values
+
+# # build the model using glm function
+# logit_model <- glm(num ~ ., data = Heart, family = binomial(link="logit"))
+
+# # summarize the model
+# summary(logit_model)
+
+# # remove the variables with higher p-values
+# logit_model_reduced <- glm(num ~ .,data = Heart, family = binomial(link = "logit"))
+
+# # using the step function on the full model
+# step(logit_model)
+
+# # now summarize again
+# summary(logit_model_reduced)
+
+# # now let's plot the fitted probability as a function of the linear predictor
+# lr_data <- data.frame(predictor=logit_model_reduced$linear.predictors, prob=logit_model_reduced$fitted.values, num = Heart$AHD)
+# ggplot(lr_data, aes(x=predictor, y=prob, color=Species)) + geom_point()
+
+# # partition our new dataset
+# train.flag_sub <- createDataPartition(y = Heart$AHD, p=0.7, list=FALSE)
+
+# # set the training data
+# training_sub <- Heart[train.flag_sub,]
+
+# # set the left data to validation
+# Validation_sub <- Heart[-train.flag_sub,]
+
+# # predict
+# pred.logit <- predict(logit_model_reduced, newdata = Validation_sub, type = "response")
+
+# #the results we get from the prediction is a probability of a specific category to occur, accordingly
+# #we add prob as a 6th variable to the validation dataset so that we can compare between the predicted
+# #value and the original one
+# Validation_sub$prob <- pred.logit
+
+# # transform the probabilities into a standard format
+# my_pred <- prediction(Validation_sub$prob, Validation_sub$AHD)
+
+# # plot model ROC curve
+# my_roc<- performance(my_pred, "tpr", "fpr")
+# plot(my_roc)
+
+# # calculate the accuracy
+# my_auc <- performance(my_pred, measure = "auc")
+# my_auc@y.values
