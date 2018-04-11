55327c6a952e9572ef4da516b2d9845dd686939e
JayFu <382603080@qq.com>
2018-04-11 21:38:03 +0200

    4.11鸢尾花sklearn应用学习

--- machinelearning1.py
+++ machinelearning1.py
@@ -1,11 +1,23 @@
 from __future__ import print_function
+import sklearn
+import numpy
 from sklearn import datasets
 from sklearn.cross_validation import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
 iris = datasets.load_iris()
+# 导入鸢尾花数据集
 iris_X = iris.data
 iris_y = iris.target
 
-print(iris_X[:2, :])
-print(iris_y)
+X_train, X_test, y_train, y_test = train_test_split(iris_X, iris_y, test_size=0.3)
+# 将数据集划分成训练集和测试集，测试集占30%
+
+knn = KNeighborsClassifier()
+# 使用这个模型
+knn.fit(X_train, y_train)
+# 训练
+print(knn.predict(X_test))
+# 预测并打印
+print(y_test)
+# 预期结果
