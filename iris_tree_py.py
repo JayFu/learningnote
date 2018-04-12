21ac0ec870747ce243c8fbd274be6739a7abd799
JayFu <382603080@qq.com>
2018-04-12 23:51:47 +0200

    决策树

--- iris_tree_py.py
+++ iris_tree_py.py
@@ -15,11 +15,15 @@ iris_y = iris.target
 X_train, X_test, y_train, y_test = train_test_split(iris_X, iris_y, test_size=0.3)
 # 将数据集划分成训练集和测试集，测试集占30%
 
-knn = KNeighborsClassifier()
+Tree = tree.DecisionTreeClassifier(criterion='gini', max_depth=None, 
+    min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, 
+    max_features=None, random_state=None, max_leaf_nodes=None, 
+    min_impurity_decrease=0.0, min_impurity_split=None,
+    class_weight=None, presort=False)
 # 使用这个模型
-knn.fit(X_train, y_train)
+Tree.fit(X_train, y_train)
 # 训练
-print(knn.predict(X_test))
+print(Tree.predict(X_test))
 # 预测并打印
 print(y_test)
 # 预期结果
