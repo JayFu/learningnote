8c08a4b195f05aba9ae2d4b09e4632451dc68ace
huj2 <382603080@qq.com>
2018-07-16 12:00:17 +0800

    爬取验证码

--- 11315pa.py
+++ 11315pa.py
@@ -1,22 +1,28 @@
 import requests
 import random
+import time
+from datetime import datetime
 
 class Catpatch_get():
 
     def __init__(self):
-        self.Uurl = 'http://www.11315.com/authCode?'
+        # self.Uurl = 'http://www.11315.com/authCode?'
+        self.Uurl = 'http://jwsys.ctbu.edu.cn/CheckCode.aspx??'
     def Catch(self):
         Hheader = {
             'User-Agent':'User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)'
         }
-        for i in range(500):
+        for i in range(20):            
+            t = time.time()
             self.Uurl += str(random.randint(3, 31423141))
             # img = requests.get(self.Uurl) 
             # f.write(img.content)
             image = requests.get(self.Uurl)
-            f = open('/Users/jay_fu/tasks/%s.jpg' % i, 'ab')
+            f = open('/Users/jay_fu/tasks/%s.png' % i, 'ab')
             f.write(image.content)
             f.close()
+            print (i,t)
+
 
 if __name__ == '__main__':
     s1 = Catpatch_get()
